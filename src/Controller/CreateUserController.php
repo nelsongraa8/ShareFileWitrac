4d0747fff3cacc8ea1472a01129d4300fb4bf26b
Nelson R. Graña <87838742+nelsongraa8@users.noreply.github.com>
2022-03-17 01:53:45 +0100

    Funcionando la creacion de usuarios

---
+++ src/Controller/CreateUserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\CreateUserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class CreateUserController extends AbstractController
+{
+    #[Route('/createuser', name: 'app_create_user')]
+    public function index(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+    ): Response {
+        $user = new User();
+        $formUser = $this->createForm(CreateUserType::class, $user);
+
+        $formUser->handleRequest($request);
+        if ($formUser->isSubmitted() && $formUser->isValid()) {
+            $user->setRoles(['ROLE_USER']);
+
+            $plaintextPassword = $formUser['password']->getData();
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+            $user->setPassword($hashedPassword);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_create_user');
+        }
+
+        return $this->render('create_user/index.html.twig', [
+            'controller_name' => 'CreateUserController',
+            'form_create_user' => $formUser->createView()
+        ]);
+    }
+}
