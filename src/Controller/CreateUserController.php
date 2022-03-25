79eca0c3d8150385956a08364cf3a80abd1fe844
Nelson R. Graña <87838742+nelsongraa8@users.noreply.github.com>
2022-03-25 01:15:43 +0100

    Refactorisando el controlador de crear usuarios

--- src/Controller/CreateUserController.php
+++ src/Controller/CreateUserController.php
@@ -13,35 +13,81 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class CreateUserController extends AbstractController
 {
+    protected $request;
+    private $formUser;
+    public $userEntity;
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private ?UserPasswordHasherInterface $passwordHasher,
+    ) {
+        $this->userEntity = new User();
+    }
+
     #[Route('/createuser', name: 'app_create_user')]
     public function index(
         Request $request,
-        EntityManagerInterface $entityManager,
-        UserPasswordHasherInterface $passwordHasher,
     ): Response {
+        $this->request = $request;
+
         $user = new User();
-        $formUser = $this->createForm(CreateUserType::class, $user);
 
-        $formUser->handleRequest($request);
-        if ($formUser->isSubmitted() && $formUser->isValid()) {
-            $user->setRoles(['ROLE_USER']);
+        $this->methodFormView();
 
-            $plaintextPassword = $formUser['password']->getData();
-            $hashedPassword = $passwordHasher->hashPassword(
-                $user,
-                $plaintextPassword
-            );
-            $user->setPassword($hashedPassword);
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
+        if ($this->methodFormSubbmited($user)) {
             return $this->redirectToRoute('app_list_file');
         }
 
         return $this->render('create_user/index.html.twig', [
             'controller_name' => 'CreateUserController',
-            'form_create_user' => $formUser->createView()
+            'form_create_user' => $this->formUser->createView()
         ]);
+    }
+
+    /**
+     * Método con el que se le pasa el formulario a
+     * la plantilla en Twig
+     *
+     * @param User $user
+     * @return void
+     */
+    private function methodFormView()
+    {
+        $this->formUser = $this
+            ->createForm(
+                CreateUserType::class,
+                $this->userEntity
+            );
+    }
+
+    /**
+     * Método con el que se procesa el Submit del formulario
+     * que se carga desde este mismo controladro en la vista
+     *
+     * @param User $user
+     * @return void
+     */
+    private function methodFormSubbmited($user)
+    {
+        $this->formUser
+            ->handleRequest(
+                $this->request
+            );
+
+        if ($this->formUser->isSubmitted() && $this->formUser->isValid()) {
+            $user->setRoles(['ROLE_USER']);
+
+            $plaintextPassword = $this->formUser['password']->getData();
+            $hashedPassword = $this->passwordHasher->hashPassword(
+                $user,
+                $plaintextPassword
+            );
+            $user->setPassword($hashedPassword);
+
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return true; // True para submit en el formulario
+        }
     }
 }
